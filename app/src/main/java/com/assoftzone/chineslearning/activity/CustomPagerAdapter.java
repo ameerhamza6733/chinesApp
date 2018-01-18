a45f1339a88d38409b50435100fcc80215fb5d49
somia <somiawaseem5@gmail.com>
2018-01-18 19:33:54 +0500

    adding pinyin v2

---
+++ app/src/main/java/com/assoftzone/chineslearning/activity/CustomPagerAdapter.java
@@ -0,0 +1,62 @@
+package com.assoftzone.chineslearning.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Somia on 1/18/2018.
+ */
+
+class CustomPagerAdapter extends PagerAdapter {
+    private Context mContext;
+    private static String TAG="CustomPagerAdapter";
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+        ModelObject modelObject = ModelObject.values()[position];
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResId(), collection, false);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             switch (view.getTag().toString()){
+                 case "select_pinyin_card":
+                     view.getContext().startActivity(new Intent(view.getContext(),intialsActivty.class));
+                     break;
+             }
+            }
+        });
+        collection.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return ModelObject.values().length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        ModelObject customPagerEnum = ModelObject.values()[position];
+        return mContext.getString(customPagerEnum.getTitleResId());
+    }
+}
