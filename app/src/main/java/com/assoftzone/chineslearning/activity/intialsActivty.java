4086bb33f3fbde1053fcdeeeef1b0bc1bf3d102c
somia <somiawaseem5@gmail.com>
2018-01-18 19:32:29 +0500

    adding pinyin

---
+++ app/src/main/java/com/assoftzone/chineslearning/activity/intialsActivty.java
@@ -0,0 +1,15 @@
+package com.assoftzone.chineslearning.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.assoftzone.chineslearning.R;
+
+public class intialsActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intials_activty);
+    }
+}
